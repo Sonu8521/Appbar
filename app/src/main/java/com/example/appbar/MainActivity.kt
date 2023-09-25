73d301d8072c83ea6dd10b17e59c392c1c20ab56
Sonu Kumar <sy199733@gmail.com>
2023-09-25 16:27:15 +0530

    first commit

---
+++ app/src/main/java/com/example/appbar/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.appbar
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//
+    }
+}
